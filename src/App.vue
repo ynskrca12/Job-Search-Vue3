787c6daf0b025b732a2d6196b5470b64f7d671df
ynskrca12 <ynskrca52@outlook.com>
2023-01-02 22:56:12 +0300

    first

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="wrapper">
+    <HelloWorld msg="You did it!" />
+    <HelloWorld msg="You did it!" />
+  </div>
+</template>
+
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
