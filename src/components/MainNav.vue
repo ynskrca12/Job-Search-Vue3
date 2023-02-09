9488659877d50c602b2f94e880ed19af5ea64404
ynskrca12 <ynskrca52@outlook.com>
2023-02-09 20:38:01 +0300

    First Vue Tests

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,9 +1,9 @@
 <template>
     <header class="w-full text-sm">
         <div class="fixed top-0 left-0 w-full h-16 bg-white">
-            <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8">
+            <div 
+            class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8">
                 <a href="/" class="flex h-full items-center text-xl">{{ company }} </a>
-                <h2 class="ml-8 flex h-full items-center">Develop by {{ author.firstName }} {{ author.lastName }}</h2>
             </div>  
         </div>
     </header>
@@ -15,10 +15,6 @@
         data () {
             return {
                 company:"Kirca Careers",
-                author:{
-                    firstName:"Yunus",
-                    lastName:"Kirca"
-                }
             };
         }
     };
---
+++ src/tests/unit/components/MainBav.test.js
@@ -0,0 +1,11 @@
+import {renderr, screen} from "@testing-library/vue";
+
+import MainNav from "@/components/MainNav.vue";
+
+describe("MainNav", () => {
+    it("displays company name", () => {
+        render(MainNav);
+    const companyName = screen.getByText("Bobo Careers");
+    expect(companyName).toBeInTheDocument();
+    });
+});
